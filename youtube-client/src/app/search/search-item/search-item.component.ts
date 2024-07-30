831f7221539a4b4a00eaaa00dcba84a87e81d241
Roman <neudakhroman@gmail.com>
2024-07-30 21:13:41 +0200

    feat: add colored border

--- youtube-client/src/app/search/search-item/search-item.component.ts
+++ youtube-client/src/app/search/search-item/search-item.component.ts
@@ -5,11 +5,19 @@ import { CommonModule } from '@angular/common';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import Item from '../search-item.model';
 import CustomButtonComponent from '../../core/components/custom-button/custom-button.component';
+import ColoredBorderDirective from '../../directives/colored-border.directive';
 
 @Component({
   selector: 'app-search-item',
   standalone: true,
-  imports: [MatTooltipModule, CommonModule, MatIconModule, MatCardModule, CustomButtonComponent],
+  imports: [
+    MatTooltipModule,
+    CommonModule,
+    MatIconModule,
+    MatCardModule,
+    CustomButtonComponent,
+    ColoredBorderDirective,
+  ],
   templateUrl: './search-item.component.html',
   styleUrl: './search-item.component.scss',
 })
